de07378c8d869caa6a7e71e2a271104fcc24983f
Mazin Artem <minima1s@yandex.ru>
2024-03-20 23:25:40 +0300

    fix: добавить редирект на main page

--- app/(layout)/page.tsx
+++ app/(layout)/page.tsx
@@ -1,29 +1,8 @@
 import * as React from 'react';
-import { Button, Rating, Tag } from '@/components';
 import styles from './page.module.css';
+import { redirect } from 'next/navigation';
 
 export default async function Home() {
-  return (
-    <main className={styles.main}>
-      <Button appearance='primary'>primary</Button>
-      <Button appearance='default'>default</Button>
-      <Rating
-        rating={2}
-        isEditable
-      />
-      <Tag>test</Tag>
-      <Tag
-        size='small'
-        color='green'>
-        test
-      </Tag>
-      <Tag
-        size='medium'
-        color='grey'>
-        test
-      </Tag>
-      <Tag color='primary'>test</Tag>
-      <Tag color='red'>test</Tag>
-    </main>
-  );
+  redirect('/courses/financial-analytics');
+  return <main className={styles.main}></main>;
 }
